26556a345bab8519ff0960987e818fc3390bb544
index <han93851207@gmail.com>
2024-07-16 01:14:27 +0900

    Sleep

--- Main/main.cpp
+++ Main/main.cpp
@@ -3,6 +3,9 @@
 
 int main()
 {
+	using namespace std::chrono_literals;
+	std::this_thread::sleep_for(500ms); // Wait for LogServer
+
 	AccountServerInstance = new AccountServer;
 	AccountServerInstance->Run();
 
